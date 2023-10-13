aae86922b4afec226b69ef213fc25c6fae951340
victorplbad <84019887+victorplbad@users.noreply.github.com>
2023-10-13 13:01:40 +0200

    1310
    kan måske teste netværk

--- Castle Bots/Assets/Scripts/PlayerMoveTest.cs
+++ Castle Bots/Assets/Scripts/PlayerMoveTest.cs
@@ -1,40 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.AccessControl;
 using UnityEngine;
 
 public class PlayerMoveTest : MonoBehaviour
 {
    public GameObject player;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int speed = 1;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position;
+       
+        
+            transform.position = player.transform.position;
 
          if (Input.GetKey(KeyCode.A))
          {
-            transform.Rotate(new Vector3(0, -1) * Time.deltaTime * 100, Space.World);
+            transform.Rotate(new Vector2(0, -1) * Time.deltaTime * speed);
          }
 
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Rotate(new Vector3(1, 0) * Time.deltaTime * 100, Space.World);
+            transform.Rotate(new Vector2(1, 0) * Time.deltaTime * speed);
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Rotate(new Vector3(-1, 0) * Time.deltaTime * 100, Space.World);
+            transform.Rotate(new Vector2(-1, 0) * Time.deltaTime * speed);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(new Vector3(0, 1) * Time.deltaTime * 100, Space.World);
+            transform.Rotate(new Vector2(0, 1) * Time.deltaTime * speed);
         }
 
 
